c64e256279ef5cf483f35d71bc6f44045628e482
cristiansuzuki <cristiansuzuki5@hotmail.com>
2024-01-13 12:23:17 -0500

    api sending data to template

--- py_rune/views.py
+++ py_rune/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import *
+from OSRSBytes import Hiscores
 
 # Create your views here.
 
@@ -7,8 +8,17 @@ def index(request):
     if request.method == 'POST': #verificar se o form foi comitado
         form = UsernameForm(request.POST)
         if form.is_valid():
+            
+            data = Hiscores(form.cleaned_data['nick'])
+            attack = data.skill('attack', 'level')
+            strength = data.skill('strength', 'level')
+            defense = data.skill('defense', 'level')
+            ranged = data.skill('ranged', 'level')
+            magic = data.skill('magic', 'level')
+            print(attack,defense,strength,ranged,magic)
             form.save()
     else:
         form = UsernameForm()
+        data = ()
 
-    return render(request,'index.html', {'form': form})
+    return render(request,'index.html', {'form': form, 'data': data, 'attack': attack})
